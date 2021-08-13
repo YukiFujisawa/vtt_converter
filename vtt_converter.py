3696388478a178c24dccc2f879265db9c8d4b058
yuki_fujisawa <g031y148yuki@gmail.com>
2021-08-13 11:19:45 +0900

    Add 初期コミット

---
+++ vtt_converter.py
@@ -0,0 +1,27 @@
+import glob
+import csv
+import os
+
+def replace_contents(contents: list) -> str:
+    del contents[3:]
+    return ''.join(contents)
+
+def replace_with_vtt_file_name(file_name: str) -> str:
+    names = file_name.split('_')
+    return names[0].replace('inputs', 'outputs') + '_' + names[1] + '_' + names[2] + '_' + names[3][0] + '_ja.vtt'
+
+def main() -> None:
+    tsv_file_names = glob.glob("./inputs/*.tsv")
+    for tsv_file_name in tsv_file_names:
+        with open(tsv_file_name, 'r') as tsv_file:
+            vtt_file_name = replace_with_vtt_file_name(tsv_file_name)
+            os.remove(vtt_file_name)
+            for contents in csv.reader(tsv_file, delimiter='\t'):
+                content = replace_contents(contents)
+                with open(vtt_file_name, 'a') as vtt_file:
+                    print(content, file=vtt_file)
+
+if __name__ == "__main__":
+    main()
+
+
